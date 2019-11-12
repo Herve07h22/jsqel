75fd89e19cc066fed9641ac1281196fac6c941d9
Herve07h22 <herve.rincent@free.fr>
2019-11-12 12:19:38 +0100

    added logger

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(cors(corsOptions))
 var queriesStore = {}
 var SECRET
 
+var logger = console.log
 
 const encrypt = text => {
     var hmac = crypto.createHmac('sha256', SECRET) // Single use
@@ -28,27 +29,26 @@ const checkAuthorization = (query, params, headers ) => {
     if (query.restricted.includes("Public")) return [true, params]
 
     if (headers.authorization && headers.authorization.split(' ')[0] === 'Bearer') {
-        console.log('Authorization : ', headers.authorization)
+        logger('Authorization : ', headers.authorization)
 
         let token = headers.authorization.split(' ')[1]
-        console.log('token : ', token)
+        logger('token : ', token)
 
         let decodedToken = token && token.split('.')
-        console.log('decodedToken : ', decodedToken)
+        logger('decodedToken : ', decodedToken)
 
         // First part is base64 encoded object, which is liable to be a fake one
         let decodedData = JSON.parse(Buffer.from(decodedToken[0], 'base64').toString('utf8')) 
-        console.log('decodedData : ', decodedData)
+        logger('decodedData : ', decodedData)
 
         // So let's sign it and compare the result
         let signedData = encrypt(decodedToken[0])
-        console.log('signedData : ', signedData)
-        console.log('decodedToken[1] : ', decodedToken[1])
+        logger('signedData : ', signedData)
+        logger('decodedToken[1] : ', decodedToken[1])
 
         if (signedData === decodedToken[1]) {
             // This is ok
-            console.log('This is ok ')
-            console.log('Expiration Checking:', decodedData.exp && Number(decodedData.exp) > Date.now())
+            logger('Expiration Checking:', decodedData.exp && Number(decodedData.exp) > Date.now())
             // Is the token still valid and provides an role granted to execute the query ?
             if (decodedData.exp && Number(decodedData.exp) > Date.now() && decodedData.role && query.restricted.includes(decodedData.role) ) {
                 return [true, Object.assign({}, decodedData, params ) ]
@@ -68,17 +68,17 @@ const createApiRoute = (app, apiUrlBase) =>
             // Clone json body request containing parameters, because beforeQueryHook may add some parameters
             let params = Object.assign({}, req.body)
             
-            console.log("Controller is processing :", query, params)
+            logger("Controller is processing :", query, params)
             
             // Check if restricted
             const [ authorized, paramsWithCredentials] = checkAuthorization(query, params, req.headers )
-            console.log("Authorized ? :", authorized, paramsWithCredentials)
+            logger("Authorized ? :", authorized, paramsWithCredentials)
 
             if (authorized) {
                 
                 // Is it a direct route ?
                 if (query.route) {
-                    console.log("Processing direct route")
+                    logger("Processing direct route")
                     // Add credentials
                     req.paramsWithCredentials = paramsWithCredentials
                     // Add a hook to execute queries
@@ -87,35 +87,35 @@ const createApiRoute = (app, apiUrlBase) =>
                 }
 
                 // Execute validation tobuild an array like [ {success, key, value, message}, {success, key, value, message} ]
-                console.log("query.params :", query.params)
+                logger("query.params :", query.params)
                 const paramsValidation = query.params ? Object.keys(query.params).map( key => Object.assign({},query.params[key](paramsWithCredentials[key]),{key}) ) : []
-                console.log("paramsValidation :", paramsValidation)
+                logger("paramsValidation :", paramsValidation)
 
                 // If anything wrong, send an error
                 const paramsErrors = paramsValidation.filter( result => result.success !== true  )
                 if (paramsErrors && paramsErrors.length) {
-                    console.log("paramsErrors :", paramsErrors)
+                    logger("paramsErrors :", paramsErrors)
                     res.status(400).json(paramsErrors)
                     return
                 } else {
                     // update params with their validator function
                     const validatedParams = paramsValidation.reduce( (acc, val) => Object.assign({}, acc, { [val.key] : val.value } ) , {}  )
-                    console.log("validatedParams :", validatedParams)
+                    logger("validatedParams :", validatedParams)
 
                     // Process params if there is any beforeQuery hook
                     const paramsProcessedByBeforeQuery = query.beforeQuery ? query.beforeQuery(query, validatedParams, { jsqeldb, encrypt }) : validatedParams
-                    console.log("paramsProcessedByBeforeQuery :", paramsProcessedByBeforeQuery)
+                    logger("paramsProcessedByBeforeQuery :", paramsProcessedByBeforeQuery)
 
                     // Alter query if there is alterQuery hook
                     const sqlQuery = query.alterQuery ? query.alterQuery(query, validatedParams, { jsqeldb, encrypt }) : query.sql
 
                     // Compute the query
                     const queryResult = await jsqeldb.executeQuery(sqlQuery, paramsProcessedByBeforeQuery)
-                    console.log("queryResult :", queryResult)
+                    logger("queryResult :", queryResult)
 
                     // Process results if there is any beforeQuery hook
                     const resultsProcessedByAfterQuery = query.afterQuery ? query.afterQuery(query, validatedParams, queryResult, { jsqeldb, encrypt }) : queryResult
-                    console.log("resultsProcessedByAfterQuery :", resultsProcessedByAfterQuery)
+                    logger("resultsProcessedByAfterQuery :", resultsProcessedByAfterQuery)
 
                     // And send, taking an optional status in the results 
                     // If the query includes a template file : send the render. If not, send the json
@@ -130,14 +130,14 @@ const createApiRoute = (app, apiUrlBase) =>
             res.status(401).send('Unauthorized')
         }
         catch(error) {
-            console.log(error)
+            logger(error)
             res.status(400).send(error)
         }
     })
 
 // TODO : add a configuration object to queries
 const registerQuery = (namespace, query, apiUrlBase) => {
-    console.log("Registering ", namespace, query.name, apiUrlBase) 
+    logger("Registering ", namespace, query.name, apiUrlBase) 
     if (!queriesStore[namespace]) queriesStore[namespace] = {}
     queriesStore[namespace][query.name] = Object.assign( {}, query)
     // Is it a direct route ?
@@ -146,7 +146,9 @@ const registerQuery = (namespace, query, apiUrlBase) => {
 
 module.exports = ({dbUri , secret , debug=false, apiUrlBase='', staticPath = ''}) => {
     
-    console.log('staticPath:', staticPath)
+    logger = debug ? console.log : ()=>null
+    logger('staticPath is', staticPath)
+
     if (Array.isArray(staticPath)) {
         staticPath.forEach( s => app.use(s.route, express.static(s.path)))
     } else {
