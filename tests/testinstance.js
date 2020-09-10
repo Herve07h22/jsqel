9f061548662997033fc217cba406176371a8de7e
Herve07h22 <herve.rincent@free.fr>
2020-09-10 12:40:48 +0200

    added proper launch of the test instance

--- tests/testinstance.js
+++ tests/testinstance.js
@@ -8,6 +8,23 @@ const hello = {
   restricted: ["Public"],
 };
 
+const hello_js = {
+  name: "hello_js",
+  sql: `SELECT 'Should be ignored;`,
+  js: () => ({ success: true }),
+  restricted: ["Public"],
+};
+
+const hello_parameter = {
+  name: "hello_parameter",
+  js: (parameters) => ({ success: true, results: parameters.test_id }),
+  restricted: ["Public"],
+  params: {
+    test_id: (value) =>
+      value ? { success: true, value } : { success: false, message: "test_id is missing or is zero" },
+  },
+};
+
 const app = jsqel({
   dbUri: databaseUri,
   secret: "anysecretkeyyouwant",
@@ -15,5 +32,5 @@ const app = jsqel({
   apiUrlBase: "",
 });
 
-app.register((namespace = "test"), [hello]);
+app.register((namespace = "test"), [hello, hello_js, hello_parameter]);
 app.run(5000);
